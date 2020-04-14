116a6438a37f13fbb8ab1e4cc3e9e06b4256ad61
riolly <albert.r.cahyantara@gmail.com>
2020-04-14 23:52:09 +0700

    minor change

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 'use strict';
+// Buat dulu database dengan nama school dan jalankan setup-seeding.js
 const express = require('express');
 const router = require('./routes');
 
--- setup-seeding.js
+++ setup-seeding.js
@@ -1,4 +1,5 @@
 // FROM JSON TO DATABASE
+// Buat dulu database dengan nama school
 const client = require('./db/config');
 const DataInit = require('./db/databaseInit');
 const filePath = ['./datas/students.json', './datas/teachers.json', './datas/subjects.json'];
